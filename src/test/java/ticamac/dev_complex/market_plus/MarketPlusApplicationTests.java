14b3b5d4c2a726941c3a6582369400a888c01727
Kamga Miguel Dimitri <kamgamigueldimitri@gmail.com>
2026-03-23 02:01:35 +0100

    fix: add missing stripe/momo properties for test profile

--- src/test/java/ticamac/dev_complex/market_plus/MarketPlusApplicationTests.java
+++ src/test/java/ticamac/dev_complex/market_plus/MarketPlusApplicationTests.java
@@ -2,12 +2,13 @@ package ticamac.dev_complex.market_plus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MarketPlusApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
